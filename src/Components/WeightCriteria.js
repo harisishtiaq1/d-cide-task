bef917f397ee9bd86346f80f7646fe9bd150e6e6
harisishtiaq1 <harisharry232@gmail.com>
2023-03-14 14:06:39 +0500

    Updated

--- src/Components/WeightCriteria.js
+++ src/Components/WeightCriteria.js
@@ -3,8 +3,32 @@ import React from "react";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import { Slider } from "@mui/material";
 import { Paper } from "@mui/material";
+import {
+  Modal,
+  ListItem,
+  List,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import CircleIcon from "@mui/icons-material/Circle";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  color: "color.default",
+  background: "background.default",
+  backgroundColor: "backgroundColor.default",
+  boxShadow: 24,
+  borderRadius: "20px",
+  p: 4,
+};
 function WeightCritertia() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
+    <>
     <Container>
       <Box
         sx={{
@@ -27,7 +51,7 @@ function WeightCritertia() {
                 "3px 3px 6px rgb(0 0 0 / 25%), -3px -3px 6px rgb(255 255 255 / 5%)",
               }}
             >
-              <QuestionMarkIcon />
+              <QuestionMarkIcon onClick={handleOpen} />
             </IconButton>
           </Tooltip>
         </Box>
@@ -77,6 +101,89 @@ function WeightCritertia() {
         </Box>
       </Box>
     </Container>
+    <Paper
+    sx={{
+      background: "background.default",
+      backgroundColor: "backgroundColor.default",
+    }}
+  >
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Tooltip title="Close">
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              boxShadow:
+                "3px 3px 6px rgb(0 0 0 / 25%), -3px -3px 6px rgb(255 255 255 / 6%)",
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Tooltip>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Decision Options
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Write every decision option you need to decide for.
+          <Box component="br" />
+          For example, if you want to make an investment, your decision
+          options may look like this:
+        </Typography>
+        <List>
+          <ListItem>
+            <IconButton sx={{ width: "20px" }}>
+              <CircleIcon sx={{ fontSize: "10px" }} />
+            </IconButton>
+            Invest In gold
+          </ListItem>
+          <ListItem>
+            <IconButton sx={{ width: "20px" }}>
+              <CircleIcon sx={{ fontSize: "10px" }} />
+            </IconButton>
+            Invest In Shares
+          </ListItem>
+          <ListItem>
+            <IconButton sx={{ width: "20px" }}>
+              <CircleIcon sx={{ fontSize: "10px" }} />
+            </IconButton>
+            Invest In Real Estate
+          </ListItem>
+        </List>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Take some time to think about every other option "out of the box"
+          that may also exist.
+          <Box component="br" />
+          <Box component="br" />
+          In case you have a binary decision (yes/no), you should write two
+          decision options. For example:
+        </Typography>
+        <List>
+          <ListItem>
+            <IconButton sx={{ width: "20px" }}>
+              <CircleIcon sx={{ fontSize: "10px" }} />
+            </IconButton>
+            Invest Money
+          </ListItem>
+          <ListItem>
+            <IconButton sx={{ width: "20px" }}>
+              <CircleIcon sx={{ fontSize: "10px" }} />
+            </IconButton>
+            Do not Invest Money
+          </ListItem>
+        </List>
+      </Box>
+    </Modal>
+  </Paper>
+  </>
   );
 }
 
