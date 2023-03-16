05787a5fdb399b02662c5728558048fbe5f6885a
harisishtiaq1 <harisharry232@gmail.com>
2023-03-16 15:55:41 +0500

    updated

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -7,18 +7,15 @@ import {
   Alert,
   Grid,
   Stack,
-  ListItem,
-  List,
   Fade,
   Snackbar,
 } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import CircleIcon from "@mui/icons-material/Circle";
+
 import React, { useEffect } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import AddIcon from "@mui/icons-material/Add";
-import Modal from "@mui/material/Modal";
+import DashboardModal from "./Modals/DashboardModal";
 import { styled } from "@mui/material/styles";
 
 import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
@@ -32,19 +29,7 @@ const LightTooltip = styled(({ className, ...props }) => (
     fontSize: 11,
   },
 }));
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 800,
-  color: "color.default",
-  background: "background.default",
-  backgroundColor: "backgroundColor.default",
-  boxShadow: 24,
-  borderRadius: "20px",
-  p: 4,
-};
+
 function Main({ handleTrigger }) {
   const [checked, setChecked] = React.useState(true);
   const handleChange = () => {
@@ -56,9 +41,6 @@ function Main({ handleTrigger }) {
   };
   const [create, setCreate] = React.useState("");
   const [newCreate, setnewCreate] = React.useState("");
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   const [Entries, setEntries] = React.useState(
     JSON.parse(localStorage.getItem("Entries") || [])
   );
@@ -121,7 +103,9 @@ function Main({ handleTrigger }) {
                   backgroundColor: "Background.default",
                 }}
               >
-                <QuestionMarkIcon onClick={handleOpen} />
+                <QuestionMarkIcon
+                // onClick={handleOpen}
+                />
               </IconButton>
             </LightTooltip>
           </Stack>
@@ -233,7 +217,9 @@ function Main({ handleTrigger }) {
                     "3px 3px 6px rgb(0 0 0 / 25%), -3px -3px 6px rgb(255 255 255 / 6%)",
                 }}
               >
-                <QuestionMarkIcon onClick={handleOpen} />
+                <QuestionMarkIcon
+                // onClick={handleOpen}
+                />
               </IconButton>
             </LightTooltip>
           </Stack>
@@ -353,89 +339,6 @@ function Main({ handleTrigger }) {
           </Alert>
         </Snackbar>
       )}
-
-      <Paper
-        sx={{
-          background: "background.default",
-          backgroundColor: "backgroundColor.default",
-        }}
-      >
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <LightTooltip title="Close">
-              <IconButton
-                aria-label="close"
-                onClick={handleClose}
-                sx={{
-                  position: "absolute",
-                  right: 8,
-                  top: 8,
-                  boxShadow:
-                    "3px 3px 6px rgb(0 0 0 / 25%), -3px -3px 6px rgb(255 255 255 / 6%)",
-                }}
-              >
-                <CloseIcon />
-              </IconButton>
-            </LightTooltip>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Decision Options
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Write every decision option you need to decide for.
-              <Box component="br" />
-              For example, if you want to make an investment, your decision
-              options may look like this:
-            </Typography>
-            <List>
-              <ListItem>
-                <IconButton sx={{ width: "20px" }}>
-                  <CircleIcon sx={{ fontSize: "10px" }} />
-                </IconButton>
-                Invest In gold
-              </ListItem>
-              <ListItem>
-                <IconButton sx={{ width: "20px" }}>
-                  <CircleIcon sx={{ fontSize: "10px" }} />
-                </IconButton>
-                Invest In Shares
-              </ListItem>
-              <ListItem>
-                <IconButton sx={{ width: "20px" }}>
-                  <CircleIcon sx={{ fontSize: "10px" }} />
-                </IconButton>
-                Invest In Real Estate
-              </ListItem>
-            </List>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Take some time to think about every other option "out of the box"
-              that may also exist.
-              <Box component="br" />
-              <Box component="br" />
-              In case you have a binary decision (yes/no), you should write two
-              decision options. For example:
-            </Typography>
-            <List>
-              <ListItem>
-                <IconButton sx={{ width: "20px" }}>
-                  <CircleIcon sx={{ fontSize: "10px" }} />
-                </IconButton>
-                Invest Money
-              </ListItem>
-              <ListItem>
-                <IconButton sx={{ width: "20px" }}>
-                  <CircleIcon sx={{ fontSize: "10px" }} />
-                </IconButton>
-                Do not Invest Money
-              </ListItem>
-            </List>
-          </Box>
-        </Modal>
-      </Paper>
     </>
   );
 }
