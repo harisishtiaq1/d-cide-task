4f0f896503f1e9c0145740512dd55b1b78e2d484
harisishtiaq1 <harisharry232@gmail.com>
2023-03-17 11:53:14 +0500

    Updated

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ColorContextProvider } from "./utilis/index";
-
+import Store from "./Store/Store"
+import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ColorContextProvider>
+    {/* <Provider Store={Store}> */}
       <App />
+      {/* </Provider> */}
     </ColorContextProvider>
   </React.StrictMode>
 );
